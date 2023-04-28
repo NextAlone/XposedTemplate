6622866590ebe15f01079c8de967179711ba639f
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-04-28 17:31:52 +0800

    Initial commit

---
+++ app/src/main/java/xyz/nextalone/xposed/hook/HookEntry.kt
@@ -0,0 +1,20 @@
+package xyz.nextalone.xposed.hook
+
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+class HookEntry : IYukiHookXposedInit {
+
+    override fun onInit() = configs {
+        debugLog {
+            tag = "NextAlone"
+        }
+    }
+
+    override fun onHook() = encase {
+        // Your code here.
+    }
+}
